030352efb4cd2ad02a174cd3a1d5fb178ccbd38c
Heinz N. Gies <heinz@licenser.net>
2016-04-15 16:36:45 +0200

    Add dummy function

--- src/dqe_idx_ddb.erl
+++ src/dqe_idx_ddb.erl
@@ -2,7 +2,7 @@
 -behaviour(dqe_idx).
 
 %% API exports
--export([lookup/1]).
+-export([lookup/1, add/5, delete/5]).
 
 %%====================================================================
 %% API functions
@@ -13,6 +13,12 @@ lookup({B, M}) ->
     {ok, [{B, dproto:metric_from_list(M)}]};
 lookup({B, M, _Where}) ->
     {ok, [{B, dproto:metric_from_list(M)}]}.
+
+add(_,_,_,_,_) ->
+    {ok, {0,0}}.
+
+delete(_,_,_,_,_) ->
+    ok.
 
 %%====================================================================
 %% Internal functions
